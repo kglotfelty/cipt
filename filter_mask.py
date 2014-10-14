b51186308b3d43d8685ddc855ff7282930205dcc
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2014-10-14 08:59:49 -0400

    Added a loop to try plotting ellipeses with fewer data points if chips complains about degenerate polygons

--- filter_mask.py
+++ filter_mask.py
@@ -42,7 +42,6 @@ class FilterMask(object):
         """
         return _region.regInsideRegion( self.region_obj, xx, yy )
     
-
     def write(self, filename):        
         with open(filename, "w") as fp:
             fp.write( self.__repr__() )
@@ -91,6 +90,9 @@ class FilterMask(object):
     def __str__(self):
         return self.__repr__()
         
+    def plot(self):
+        raise NotImplementedError("Plotting not supported at this level")
+        
 
 class FilterMaskDefbyRadiusAngle(FilterMask):
     """
@@ -118,17 +120,24 @@ class FilterMaskDefbyRadiusAngle(FilterMask):
         retval = "{}({})".format( self.isa, inner )
         return retval
 
+
 class Box( FilterMaskDefbyRadiusAngle ):
     def __init__(self, xlen, ylen=None, center=None, angle=None ):        
         # todo (x,(cx,cy)) should also be workable
         ylen = xlen if ylen is None else ylen
         self.isa = "box"
         FilterMaskDefbyRadiusAngle.__init__( self, xlen, ylen, center, angle)
+        
+    def plot(self):
+        _plot_box( self.xcenter, self.ycenter, self.xlen, self.ylen, self.angle )
+        
 
 class Ellipse( FilterMaskDefbyRadiusAngle ):
     def __init__(self, xlen, ylen, center=None, angle=0 ):        
         self.isa = "ellipse"
         FilterMaskDefbyRadiusAngle.__init__( self, xlen, ylen, center, angle)
+    def plot( self ):
+        _plot_ellipse( self.xcenter, self.ycenter, self.xlen, self.ylen, self.angle )
 
 class Annulus( FilterMaskDefbyRadiusAngle ):
     def __init__(self, inner, outer, center=None ):        
@@ -139,11 +148,15 @@ class Circle( FilterMaskDefbyRadiusAngle ):
     def __init__(self, inner, center=None ):        
         self.isa = "circle"
         FilterMaskDefbyRadiusAngle.__init__( self, inner, center=center)
+    def plot(self):
+        _plot_ellipse( self.xcenter, self.ycenter, self.xlen, self.xlen, 0 )
             
 class Point( FilterMaskDefbyRadiusAngle ):
     def __init__(self, center=None ):        
         self.isa = "point"
         FilterMaskDefbyRadiusAngle.__init__( self, None, center=center)
+    def plot(self):
+        pass # points not plotted
 
 class Pie( FilterMaskDefbyRadiusAngle ):
     def __init__(self, xlen, ylen, start,stop, center=None ):        
@@ -160,6 +173,12 @@ class Rectangle( FilterMaskDefbyRadiusAngle ):
         self.isa = "rectangle"
         FilterMaskDefbyRadiusAngle.__init__( self, xmax, ylen=ymax, center=(xmin,ymin) )
 
+    def plot(self):
+        dx = self.xmax-self.xmin
+        dy = self.ymax-self.ymin
+        xc = (self.xmax+self.xmin)/2.0
+        yc = (self.ymax+self.ymin)/2.0
+        _plot_box( xc, yc, dx, dy, 0.0 )
 
 class Field( FilterMask ):
     def __init__(self):
@@ -168,6 +187,9 @@ class Field( FilterMask ):
 
     def __repr__(self):
         return "field()"
+        
+    def plot(self):
+        pass  # fields are not plotted
     
     
 class Polygon(FilterMask):
@@ -190,6 +212,9 @@ class Polygon(FilterMask):
     def __repr__(self):
         coords = ",".join(map(str, self.polypoints))        
         return "{}({})".format( self.isa, coords )
+        
+    def plot(self):
+        _plot_polygon(self.polypoints)
 
     
 class Region(FilterMask):
@@ -261,3 +286,92 @@ def region( filename ):
     """
     return Region(filename)
     
+# ---------------------------
+# --------------------------------------
+#
+
+try:
+    from pychips import add_region
+except:
+    def add_region( *arg, **kwargs):
+        raise NotImplementedError("Chips is not installed.  Plotting is not supported")
+        
+import numpy as np
+
+
+def _plot_box( x0, y0, xl, yl, rotang, style="" ):
+    """
+    """
+    
+    dx=xl/2.0
+    dy=yl/2.0
+    
+    xx = np.array([ x0-dx, x0+dx, x0+dx, x0-dx])
+    yy = np.array([ y0-dy, y0-dy, y0+dy, y0+dy])
+    
+    dx = xx-x0
+    dy = yy-y0    
+    cosa = np.cos( np.deg2rad( -rotang) )
+    sina = np.sin( np.deg2rad( -rotang) )    
+    rx = ( dx * cosa + dy * sina ) + x0
+    ry = (-dx * sina + dy * cosa ) + y0
+
+    # plot it
+    add_region(rx,ry, style)
+
+
+def _plot_polygon( points, style="" ):
+    """
+    """
+    x = points[::2]
+    y = points[1::2]
+    add_region(x,y, style)
+
+
+def _plot_ellipse( x0, y0, r1, r2, rotang, style="", nbins=100 ):
+    """
+    Okay, very long way of doing this, not terrible efficent but it works.
+
+    """
+    np.seterr(all='ignore')
+    if nbins < 1:
+        raise RuntimeError("Cannot plot this shape")
+    
+    r12 = r1*r1
+    r22 = r2*r2
+    mr = max([r1,r2])
+    dx = 2.0*max([r1,r2])/nbins
+
+    # Top of ellipse
+    xp = np.arange( -r1-dx, r1+dx, dx )+x0
+    yp = y0 + np.sqrt( r22 * ( 1 - ((xp-x0)**2)/r12))
+    
+    # Bottom of ellipse
+    xm = xp[::-1]
+    ym = y0 - np.sqrt( r22 * ( 1 - ((xm-x0)**2)/r12))
+    
+    # Combine them together, omit 1st point to avoid dup, degenerate polys
+    xp = np.append(xp, xm[1:])
+    yp = np.append(yp, ym[1:])
+    
+    # Remove where Y value is outside of ellipse
+    xy = filter( lambda i: np.isfinite( i[1]), zip(xp,yp))
+    x = np.array([i[0] for i in xy])
+    y = np.array([i[1] for i in xy])
+
+    # rotation matrix
+    dx = x-x0
+    dy = y-y0    
+    cosa = np.cos( np.deg2rad( -rotang) )
+    sina = np.sin( np.deg2rad( -rotang) )    
+    rx = ( dx * cosa + dy * sina ) + x0
+    ry = (-dx * sina + dy * cosa ) + y0
+
+    # plot it
+
+    try:
+        add_region(rx,ry, style)
+    except:
+        # may be too many poits, try with fewer
+        ibins = int(0.9*nbins)
+        _plot_ellipse( x0, y0, r1, r2, rotang, style=style, nbins=ibins ):
