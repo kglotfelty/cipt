77fe5ac87efb45cf12991ffd4fdf257c48fe4dfe
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2020-12-14 09:30:24 -0500

    ciao4.13 setup changes

--- setup.py
+++ setup.py
@@ -6,17 +6,14 @@ import sys
 
 assert "ASCDS_INSTALL" in os.environ
 
-ver = sys.version_info
-os.environ["PYVER"] = "python{}.{}".format(ver[0],ver[1])
-
 from distutils.core import setup
 
 
 setup( name='CIPT',
-        version='0.0.1',
+        version='4.13.0',
         description='CIAO Image Processing Toolkit',
-        author='Anonymous',
-        author_email='kglotfelty@cfa.harvard.edu',
+        author='Kenny Glotfelty',
+        author_email='glotfeltyk@si.edu',
         url='https://github.com/kglotfelty/cipy/',
         py_modules=["__init__",
                     "all",
